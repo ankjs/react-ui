b6bab930a13dba3e873c5fad8e334a12b0b617b9
ankjs <ankdevlopment@gmail.com>
2026-04-15 16:11:42 +0530

    100101

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+
+
+import AnkThemeProvider from './providers/AnkThemeProvider';
+export default AnkThemeProvider;
+
+
+
+
+export * from './components/button/index';
+export * from './components/input/index';
